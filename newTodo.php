336df8583bbfa6b7b213960a84b0b3bd7cfe7774
UnknownCreator1 <90321114+UnknownCreator1@users.noreply.github.com>
2023-01-16 12:07:52 +0000

    can not add new todo the json file, permission denied, needs to be sorted

--- newTodo.php
+++ newTodo.php
@@ -10,9 +10,23 @@ $todoName = trim($todoName);
 if($todoName){
     // echo "save Todo";
     $json = file_get_contents("todo.json");
-    // file_put_contents("todo.json");
-    echo "<pre>";
-    echo $json;
-    echo "</pre>";
+    $jsonArray = json_decode($json, true);
+    // will return an associative array
+    // an object in json is converted into an array using json_decode
+
+    $jsonArray[$todoName] = ["completed" => false];
+    // this will insert the todoName into the associative array
+    // and for this todoName it will  correspond to an associative array of completed => false
+
+    // echo "<pre>";
+    // echo var_dump($jsonArray);
+    // echo "</pre>";
+
+    // $jsonFile = "todo.json";
+
+    file_put_contents("todo.json", json_encode($jsonArray)); 
+    // an associative array in json is converted into json using json_encode
+    // this will in result save the todo into the json file
+
 }
 ?>
